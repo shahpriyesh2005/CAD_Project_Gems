a06527d107fe825f57713ad66e9a47c4de19d33c
Priyesh Shah <55635249+shahpriyesh2005@users.noreply.github.com>
2019-12-07 19:08:23 +0000

    Changes to luhnAlgorithmCheck

--- luhnAlgorithmCheck/lib/luhnAlgorithmCheck.rb
+++ luhnAlgorithmCheck/lib/luhnAlgorithmCheck.rb
@@ -5,31 +5,10 @@ class LuhnAlgorithm
     @num = num
   end
 
-  def addends
-    raw_addends.map do |digit|
-      digit < 10 ? digit : digit - 9
-    end
+  def luhn_valid?()
+   num.scan(/\d/).reverse.each_slice(2)
+      .sum { |i, k = 0| i.to_i + ((k.to_i)*2).digits.sum }
+      .modulo(10).zero?
   end
 
-  def checksum
-    addends.inject(:+)
-  end
-
-  def valid?
-    checksum % 10 == 0
-  end
-  
-  def self.create(num)
-    [*0..9].each do |digit|
-      possible = Luhn.new("#{num}#{digit}".to_i)
-      return possible.num if possible.valid?
-    end
-  end
-
-  private
-  def raw_addends
-    num.to_s.reverse.chars.map.with_index do |digit, index|
-      index.odd? ? digit.to_i * 2 : digit.to_i
-    end.reverse
-  end
 end
--- luhnAlgorithmCheck/test/test_luhnAlgorithmCheck.rb
+++ luhnAlgorithmCheck/test/test_luhnAlgorithmCheck.rb
@@ -4,10 +4,10 @@ require 'luhnAlgorithmCheck'
 class LuhnAlgorithmTest < Minitest::Test
   # Test if the Luhn's Algorithm returns correct result
   def test_negative_luhn_algorithm
-	assert_equal(false, LuhnAlgorithm.new("1234567890123456").valid?)
+	assert_equal(false, LuhnAlgorithm.new("1234567890123456").luhn_valid?)
   end
   
   def test_positive_luhn_algorithm
-	assert_equal(true, LuhnAlgorithm.new("4012888888881881").valid?)
+	assert_equal(true, LuhnAlgorithm.new("4012888888881881").luhn_valid?)
   end
 end
